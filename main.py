c86271153d392535df695921b87c876050f12b0b
alonso <jorgealonsovasquez@hotmail.com>
2023-12-29 14:29:02 -0600

    Cambiada la estetica con un archivo json

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ app = CTk()
 app.geometry("500x500")
 
 set_appearance_mode("dark")
+set_default_color_theme("GhostTrain.json")
 
-audio = CTkFrame(master=app, fg_color="#900C3F", border_width=2)
+audio = CTkFrame(master=app, border_width=2)
 audio.pack(expand=True)
 
 label_audio = CTkLabel(master=audio, text="Descargar de youtube")
 entry = CTkEntry(master=audio, placeholder_text="Ingresa el link del video", width = 300)
-btn_audio = CTkButton(master=audio, text="Descargar audio", corner_radius=0, fg_color="#C70039", hover_color="#FF5733", border_width=2, command=lambda: process_link_audio(entry.get()))
-btn_video = CTkButton(master=audio, text="Descargar video", corner_radius=0, fg_color="#C70039", hover_color="#FF5733", border_width=2, command=lambda: process_link_video(entry.get()))
+btn_audio = CTkButton(master=audio, text="Descargar audio", corner_radius=0, border_width=2, command=lambda: process_link_audio(entry.get()))
+btn_video = CTkButton(master=audio, text="Descargar video", corner_radius=0, border_width=2, command=lambda: process_link_video(entry.get()))
 
 label_audio.pack(anchor="s", expand=True, pady=10, padx=30)
 entry.pack(anchor="s", expand=True, pady=10, padx=30)
