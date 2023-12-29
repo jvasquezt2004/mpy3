e1a945fe166d5aec01a73a4b3ba358c54047dae3
alonso <jorgealonsovasquez@hotmail.com>
2023-12-29 14:09:18 -0600

    Logica para descargar video realizada

--- tube.py
+++ tube.py
@@ -2,20 +2,32 @@ from pytube import YouTube
 import os
 
 HOME_DIR = os.environ['HOME']
-DESTINY = os.path.join(HOME_DIR, 'Audio')
+DESTINY_AUDIO = os.path.join(HOME_DIR, 'Audio')
+DESTINY_VIDEO = os.path.join(HOME_DIR, 'Video')
 
 def process_link_audio(link: str):
-    video = YouTube(link)
-    audio_stream = video.streams.filter(only_audio=True).first()
+    audio = YouTube(link)
+    audio_stream = audio.streams.filter(only_audio=True).first()
 
-    if not os.path.exists(DESTINY):
-        os.makedirs(DESTINY)
+    if not os.path.exists(DESTINY_AUDIO):
+        os.makedirs(DESTINY_AUDIO)
     
-    out_file = audio_stream.download(output_path=DESTINY)
+    out_file = audio_stream.download(output_path=DESTINY_AUDIO)
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
 
+def process_link_video(link: str):
+    video = YouTube(link)
+    if not os.path.exists(DESTINY_VIDEO):
+        os.makedirs(DESTINY_VIDEO)
+
+    video_stream = video.streams.get_highest_resolution()
+    out_file = video_stream.download(output_path=DESTINY_VIDEO)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp4'
+    os.rename(out_file, new_file)
+
 if __name__ == "__main__":
     test = input("Ingresa el link: ")
-    process_link(test)
+    process_link_video(test)
