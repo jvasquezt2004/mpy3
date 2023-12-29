9413acc586319eb2378164577ffff47b233eba05
alonso <jorgealonsovasquez@hotmail.com>
2023-12-29 14:12:34 -0600

    Agregado la logica para el boton de descargar video

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ audio.pack(expand=True)
 label_audio = CTkLabel(master=audio, text="Descargar de youtube")
 entry = CTkEntry(master=audio, placeholder_text="Ingresa el link del video", width = 300)
 btn_audio = CTkButton(master=audio, text="Descargar audio", corner_radius=0, fg_color="#C70039", hover_color="#FF5733", border_width=2, command=lambda: process_link_audio(entry.get()))
-btn_video = CTkButton(master=audio, text="Descargar video", corner_radius=0, fg_color="#C70039", hover_color="#FF5733", border_width=2, command=lambda: process_link_audio(entry.get()))
+btn_video = CTkButton(master=audio, text="Descargar video", corner_radius=0, fg_color="#C70039", hover_color="#FF5733", border_width=2, command=lambda: process_link_video(entry.get()))
 
 label_audio.pack(anchor="s", expand=True, pady=10, padx=30)
 entry.pack(anchor="s", expand=True, pady=10, padx=30)
